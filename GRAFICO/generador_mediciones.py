d29bbf5c686a34e448cb152ff11b6b028f4b158b
ColinaAndres <andrescolina725@gmail.com>
2024-05-04 12:22:25 -0300

    Grafico mas ajuste cuadratico

--- GRAFICO/generador_mediciones.py
+++ GRAFICO/generador_mediciones.py
@@ -6,8 +6,8 @@ from time import time
 
 def generar_mediciones():
     with open('Mediciones.txt','a') as file:
-        # file.write("Cantidad_de_elementos,Media_de_tiempo_de_ejecucion\n")
-        for i in range(4650,5001,50):
+        file.write("Cantidad_de_elementos,Media_de_tiempo_de_ejecucion\n")
+        for i in range(100,5001,50):
             xi,f=leer_archivo("../MAS_TESTS/"+str(i)+".txt")
             suma_de_tiempos=0
             for j in range(5):
--- GRAFICO/grafico.py
+++ GRAFICO/grafico.py
@@ -1,34 +1,39 @@
 import matplotlib.pyplot as plt
 import csv
+import numpy
+from scipy.optimize import curve_fit
 
-x=[]
-y=[]
-# x2=[]
-# y2=[]
-with open("Mediciones.txt","r") as file:
-    lector=csv.reader(file,delimiter=",")
-    file.readline()
-    for linea in lector:
-        x.append(int(linea[0]))
-        y.append(float(linea[1]))
+def leerMediciones(x,y):
+    with open("Mediciones.txt","r") as file:
+        lector=csv.reader(file,delimiter=",")
+        file.readline()
+        for linea in lector:
+            x.append(int(linea[0]))
+            y.append(float(linea[1]))
 
-# with open("TESTS/Mediciones_lineal.txt") as file:
-#     lector=csv.reader(file,delimiter=",")
-#     file.readline()
-#     for linea in lector:
-#         x2.append(int(linea[0]))
-#         y2.append(float(linea[1]))
+def funcionCuadratica(x, a, b, c):
+    return (a*x**2) + (b*x) + c
 
+def graficar():
 
-figure,ax = plt.subplots()
-ax.plot(x,y,color="Red",label="cal_sumatoria")
-ax.legend(loc='upper left')
+    x=[]
+    y=[]
 
-# ax.plot(x2,y2,color="Blue",label="encontrar_maximo")
-# ax.legend(loc='upper left')
+    leerMediciones(x,y)
 
-plt.title('Algoritmo de Maximas kills')
-plt.xlabel('Cantidad de elementos en Arreglo')
-plt.ylabel('Tiempo [ms]')
-plt.grid(True)
-plt.show()
+    res , _ = curve_fit(funcionCuadratica,x,y)
+    
+    x2 = numpy.linspace(0,5000,500)
+    
+    _ , ax = plt.subplots()
+    ax.plot(x,y,color="blue",label="Pd")
+    ax.plot(x2,funcionCuadratica(x2,res[0],res[1],res[2]),color="red",label="Ajuste cuadratico")
+    ax.legend(loc='upper left')
+
+    plt.title('Algoritmo de Maximas kills')
+    plt.xlabel('Cantidad de elementos en C/arreglo')
+    plt.ylabel('Tiempo [ms]')
+    plt.grid(True)
+    plt.show()
+
+graficar()
